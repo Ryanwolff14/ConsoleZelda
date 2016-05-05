1e78603aaa223b14a0c5e36514b9f7e433b84ecc
Ryanwolff14 <ryan.wolff14@outlook.com>
2016-05-05 16:56:09 -0500

    updated info menu

--- projectweek/InfoMenu.cs
+++ projectweek/InfoMenu.cs
@@ -11,6 +11,20 @@ namespace projectweek
         public static void info()
         {
             Console.WriteLine("Welcome");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Made by gitusername: RyanWolff14");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Not the best console game out there but will be improved in the future as i learn more about code");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("depending on when i find time to come back to it:(");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Suppose to be a similar game to the 1990s zelda that came out on the nintendo console");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("Any tips or ideas on how to make it better just comment on the github page for the application");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("thanks for playing:)");
+            System.Threading.Thread.Sleep(500);
+            Console.WriteLine("whenever your ready to start press any key");
             Console.ReadKey();
         }
     }
